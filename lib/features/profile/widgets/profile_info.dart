6a30fac8e6432a1a314ea5c3ff16ff7fdf3c12e8
sergss <sergssnorth@mail.ru>
2024-01-19 11:59:03 +0300

    add widgets account info and userphotogrid

--- lib/features/profile/widgets/profile_info.dart
+++ lib/features/profile/widgets/profile_info.dart
@@ -5,23 +5,30 @@ class ProfileInformationWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16.0),
-      child: Column(
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CircleAvatar(
             radius: 50.0,
             backgroundImage: AssetImage('avatar.jpg'), // Замените на реальный URL аватара
           ),
-          SizedBox(height: 16.0),
-          Text(
-            'Имя пользователя',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
+          SizedBox(width: 16.0), // Добавим промежуток между фото и текстом
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Имя пользователя',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text('Биография пользователя...'),
+                // Добавьте другие элементы информации о пользователе по вашему усмотрению
+              ],
             ),
           ),
-          Text('Биография пользователя...'),
-          // Добавьте другие элементы информации о пользователе по вашему усмотрению
         ],
       ),
     );
